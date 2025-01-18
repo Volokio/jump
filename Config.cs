507cb4ca3ced7e066ccabbce59fff4bb749dd7bd
Volokio <flowdestroi@gmail.com>
2025-01-18 09:30:23 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Interfaces;
+using Exiled.API.Features;
+using System.ComponentModel;
+
+
+namespace Broadcasts
+{
+    public class Config : IConfig
+    {
+        [Description("Включен ли плагин")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Включен ли дебагмод")]
+        public bool Debug { get; set; } = false;
+
+        [Description("время бродкаста")]
+        public ushort Duration { get; set; } = 1;
+
+        [Description("текст бродкаста при прыжке")]
+        public string Textjump { get; set; } = "<color=green>ты прыгнул</color>";
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.Events.Commands.PluginManager;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Server;
+using Exiled.API.Interfaces;
+using Exiled.Events.Features;
+
+namespace Broadcasts
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "Volokno";
+        public override string Name => "jump";
+        public override string Prefix => "jump";
+        public override Version Version => base.Version;
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Verified += OnVerified;
+            base.OnEnabled();
+
+            Exiled.Events.Handlers.Player.Jumping += OnPlayerJumping;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Verified -= OnVerified;
+            base.OnDisabled();
+
+            Exiled.Events.Handlers.Player.Jumping += OnPlayerJumping;
+            base.OnDisabled();        }
+        private void OnVerified(VerifiedEventArgs ev)
+        {
+            if (ev.Player == null && ev.Player.IsNPC && ev.Player.IsHost)
+                return;
+        }
+        private void OnPlayerJumping(JumpingEventArgs ev)
+        {
+
+            if (ev.Player == null)
+                return;
+
+            ev.Player.Broadcast(Config.Duration, Config.Textjump);
+        }
+
+    }
+}
